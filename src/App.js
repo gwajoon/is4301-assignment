df151c3523d928f9d286713bdbaa14e8345b7a68
gwajoon <hengguojun25@gmail.com>
2023-11-16 21:08:23 +0800

    Add google and email/password login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 import Academics from "./Academics/Academics";
 import Admission from "./Admissions/Admission";
 import Home from "./Home";
 import "./App.css";
+import Login from "./Login/Login";
+import Register from "./Register/Register";
 
 function App() {
   return (
     <Router>
+      <Login />
       <div className="App">
         <nav className="navbar">
           <Link to="/">Home</Link>
@@ -19,6 +27,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/academics" element={<Academics />} />
           <Route path="/admission" element={<Admission />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </div>
     </Router>
---
+++ src/Login/Login.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import {
+  auth,
+  logInWithEmailAndPassword,
+  signInWithGoogle,
+  logout,
+} from "../middleware/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import "./Login.css";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoggedin, setIsLoggedIn] = useState(false);
+  const [user, loading, error] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if (loading) {
+      return;
+    }
+    if (user) {
+      navigate("/");
+      setIsLoggedIn(true);
+    }
+  }, [user, loading]);
+
+  return (
+    location.pathname !== "/register" && (
+      <div className="login">
+        {!isLoggedin ? (
+          <div className="login__container">
+            <input
+              type="text"
+              className="login__textBox"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="E-mail Address"
+            />
+            <input
+              type="password"
+              className="login__textBox"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+            />
+            <button
+              className="login__btn"
+              onClick={() => logInWithEmailAndPassword(email, password)}>
+              Login
+            </button>
+            <button
+              className="login__btn login__google"
+              onClick={signInWithGoogle}>
+              Login with Google
+            </button>
+            <div>
+              <Link to="/reset">Forgot Password</Link>
+            </div>
+            <div>
+              Don't have an account? <Link to="/register">Register</Link> now.
+            </div>
+          </div>
+        ) : (
+          <button className="dashboard__btn" onClick={logout}>
+            Logout
+          </button>
+        )}
+      </div>
+    )
+  );
+}
+
+export default Login;
